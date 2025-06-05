184dd7abdd2a1dee58343af25522c8b96feec10b
alltechdev <github@alltech.dev>
2025-06-05 13:46:11 -0400

    Delete scripts/server-setup directory

--- scripts/server-setup/standard-install.sh
+++
@@ -1,7 +0,0 @@
-sudo apt update && sudo apt upgrade -y
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
-sudo mkdir -p /opt/stacks /opt/dockge
-cd /opt/dockge
-sudo curl https://raw.githubusercontent.com/louislam/dockge/master/compose.yaml --output compose.yaml
-sudo docker compose up -d
